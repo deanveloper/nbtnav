8737854e69a54211a8f11899440193e21c5c1bcf
Dean Bassett <dean@deanveloper.com>
2017-10-06 01:19:45 -0400

    added more parsing stuff

--- nbt/parser.go
+++ nbt/parser.go
@@ -1,5 +1,11 @@
 package nbt
 
+import (
+	"encoding/binary"
+	"io"
+	"unsafe"
+)
+
 func Parse(r io.Reader) (*Tag, error) {
 	buf := bufio.NewBuffer(r)
 	for {
@@ -15,16 +21,88 @@ func ReadTagType(buf *bufio.Reader) (TagType, error) {
 	return TagType(b), nil
 }
 
-func ReadName(buf *bufio.Reader) (string, error) {
-	len := binary.BigEndian.ReadUint16(buf)
-	
-	nameBytes := make([]byte, len)
-	_, err := buf.Read(nameBytes)
+func ParseInt8(buf *bufio.Reader) (int8, error) {
+	var i int8
+	err := binary.Read(buf, binary.BigEndian, &i)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+func ParseInt16(buf *bufio.Reader) (int16, error) {
+	var i int16
+	err := binary.Read(buf, binary.BigEndian, &i)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+func ParseInt32(buf *bufio.Reader) (int32, error) {
+	var i int32
+	err := binary.Read(buf, binary.BigEndian, &i)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+func ParseInt64(buf *bufio.Reader) (int64, error) {
+	var i int64
+	err := binary.Read(buf, binary.BigEndian, &i)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+func ParseFloat32(buf *bufio.Reader) (float32, error) {
+	var i float32
+	err := binary.Read(buf, binary.BigEndian, &i)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+func ParseFloat64(buf *bufio.Reader) (float64, error) {
+	var i float64
+	err := binary.Read(buf, binary.BigEndian, &i)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+func ParseInt8Array(buf *bufio.Reader) ([]int8, error) {
+	len, err := ParseInt32(buf)
+	if err != nil {
+		return nil, err
+	}
+	arr := make([]int8, len)
+	for i := 0; i < len; i++ {
+		i8, err := ReadInt8(buf)
+		if err != nil {
+			return nil, err
+		}
+		arr[i] = i8
+	}
+	return arr, nil
+}
+
+func ParseString(buf *bufio.Reader) (string, error) {
+	len, err := ParseInt16(buf)
 	if err != nil {
 		return "", err
 	}
 
-	return string(nameBytes), nil
+	arr := make([]byte, len)
+	for i := 0; i < len; i++ {
+		i8, err := ReadInt8(buf)
+		if err != nil {
+			return nil, err
+		}
+		arr[i] = byte(i8)
+	}
 }
-
-func 
